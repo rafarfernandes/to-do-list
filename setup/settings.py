3a3a1b86027b5ca2dde42f147630cad2f22830f5
Rafael Fernandes <rafaelrofernandes@gmail.com>
2024-12-03 10:39:43 -0300

    Retirando Heroku e mudando banco de dados para AWS RDS-MYSQL

--- setup/settings.py
+++ setup/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os  
 from dotenv import load_dotenv
 import environ
-import django_heroku
+
 
 env = environ.Env(
     DEBUG=(bool, False)
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
         'OPTIONS': {
             'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
@@ -154,4 +154,3 @@ LOGIN_URL = 'login_view'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-django_heroku.settings(locals())
