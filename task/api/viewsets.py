823de6e9ee79748a4bed4d74b0a272a7de91d307
Rafael Fernandes <rafaelrofernandes@gmail.com>
2024-12-03 00:06:58 -0300

    Atualizando api

--- task/api/viewsets.py
+++ task/api/viewsets.py
@@ -4,7 +4,7 @@ from .serializers import TaskSerializer
 from task.models import Task
 
 class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
+    queryset = Task.objects.all()  
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
 
@@ -12,4 +12,4 @@ class TaskViewSet(viewsets.ModelViewSet):
         return Task.objects.filter(user=self.request.user)
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        serializer.save(user=self.request.user) 
