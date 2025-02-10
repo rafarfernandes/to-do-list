8a90deec7a98f85a6f2601a7f1d72365883a116a
Rafael Fernandes <rafaelrofernandes@gmail.com>
2025-02-10 19:41:55 -0300

    Db ajustado

--- setup/settings.py
+++ setup/settings.py
@@ -87,8 +87,8 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
+        'NAME': 'db_tarefas',
+        'USER': 'root',
         'PASSWORD': env('DB_PASSWORD'),
         'HOST': env('DB_HOST'),
         'PORT': env('DB_PORT'),
